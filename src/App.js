e6b52254206a2028e28e2af5b9d785ffd5c40c7c
Muhammad Elzandi Yusuf <elzandimilanisty29@gmail.com>
2024-02-06 14:46:29 +0700

    feat: add helper calculateDiagonalSum

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useCallback, useState } from 'react';
+import { calculateDiagonalSum } from './utils/helper';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [stone, setStone] = useState({ count: 0, matrix: [] });
+
+  const handleChangeNumber = useCallback((event) => {
+    const { value } = event.target;
+    setNumber(value);
+  }, [])
+
+  const handleSubmit = useCallback(() => {
+    const results = calculateDiagonalSum(number);
+    setStone(results)
+  }, [number]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Case 1: Unearthing the Philosopher's Stone</h1>
+      <div className="app-wrapper">
+        <input className="input" type="number" placeholder="Masukan jumlah N" onChange={handleChangeNumber} />
+        <button className="button" onClick={handleSubmit}>Submit</button>
+      </div>
+      {stone.count > 0 && (
+        <>
+          <h2>Count: {stone.count}</h2>
+          <h2>Matrix:</h2>
+          {stone.matrix?.map((item, index) => (
+            <div className="list" key={index}>
+              {item?.map((size) => (
+                <div className="list-item" key={size}>{size}</div>
+              ))}
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
---
+++ src/utils/helper/index.js
@@ -0,0 +1,58 @@
+export function calculateDiagonalSum(gridSize) {
+    const size = Number(gridSize);
+    const results = Array.from({ length: gridSize }, () => []);
+
+    let row = 0;
+    let col = 0;
+    let rowEnd = size - 1;
+    let colEnd = size - 1;
+    let finalSize = size * size;
+    let sumDiagonalNumber = new Array(size + 1);
+
+    // Generate Matrix
+    while (col <= colEnd && row <= rowEnd) {
+        // Top row
+        for (let i = colEnd; i >= col; i--) {
+            results[row][i] = finalSize;
+            finalSize--;
+        }
+        row++;
+
+        // end column
+        for (let i = row; i <= rowEnd; i++) {
+            results[i][col] = finalSize;
+            finalSize--;
+        }
+        col++;
+
+        // end row
+        for (let i = col; i <= colEnd; i++) {
+            results[rowEnd][i] = finalSize;
+            finalSize--;
+        }
+        rowEnd--;
+
+        // End col from first
+        for (let i = rowEnd; i >= row; i--) {
+            results[i][colEnd] = finalSize;
+            finalSize--;
+        }
+        colEnd--;
+    }
+
+    // Sum Diagonal Matrix
+    if(size === 1) {
+        sumDiagonalNumber = 1;
+    }
+
+    sumDiagonalNumber[1] = 1;
+    sumDiagonalNumber[0] = 0;
+
+    for (let i = 2; i <= size; i++) {
+        sumDiagonalNumber[i] = (4 * (i * i)) - 6 * (i - 1) + sumDiagonalNumber[i - 2];
+    }
+
+    sumDiagonalNumber = sumDiagonalNumber[size];
+
+    return {  count: sumDiagonalNumber, matrix: results};
+}
